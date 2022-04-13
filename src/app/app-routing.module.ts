0ca1bce00f506f7350810bc4d9190bef3d7daa20
Harold Woody <89166105+HuckWoody@users.noreply.github.com>
2022-04-13 12:17:11 -0400

    Routed home page, checout, and profile in nav bar. Linked soical media in footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { OrderProfileComponent } from './order-profile/order-profile.component';
+import { CheckOutComponent } from './check-out/check-out.component';
 
-
+const routes: Routes = [
+  {
+      path: 'home',
+      component: HomePageComponent
+  },
+  {
+    path: 'checkout',
+    component: CheckOutComponent
+  },
+  {
+    path: 'profile',
+    component: OrderProfileComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
