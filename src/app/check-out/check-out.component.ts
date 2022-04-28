16ad94647585fe4d8c4ed95165e96c7d00a3389d
Harold Woody <89166105+HuckWoody@users.noreply.github.com>
2022-04-28 14:57:50 -0400

    Can delete items from cart and Submit Order function create

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Coffee } from '../models/coffee';
+import { CoffeesService } from '../services/coffees.service';
 
 @Component({
   selector: 'app-check-out',
@@ -8,8 +9,9 @@ import { Coffee } from '../models/coffee';
 })
 export class CheckOutComponent implements OnInit {
   public items: Coffee[];
+  // public coffeeKeys: any;
 
-  constructor() { }
+  constructor(private coffeesService: CoffeesService) { }
 
   ngOnInit(): void {
     let coffeeString = localStorage.getItem('items');
@@ -18,21 +20,29 @@ export class CheckOutComponent implements OnInit {
 
       this.items = JSON.parse(coffeeString);
 
+     
+    }
+  }
+    deleteItem(index: number) {
+
+        this.items.splice(index, 1);
+        localStorage.setItem("items", JSON.stringify(this.items))
+
+    }
+
+    checkoutOrder() {
       let coffeeIds = [];
 
       for (let key in this.items) {
         coffeeIds.push(this.items[key].coffee_id);
-        // console.log(this.items[key].coffee_id);
+        console.log(this.items[key].coffee_id);
       }
-
-      console.log(coffeeIds);
-
+      this.coffeesService.postOrder(coffeeIds).subscribe(response => {
+        // If statement needs to be created. Response needed from backened confirming order received then below actions can be taken.
+        this.items = [];
+        localStorage.setItem('items', "[]");
+      },
+      )
     }
-  }
   
-  deleteItems() {
-    localStorage.removeItem('items');
-    this.items = [];
-    // this.items = localStorage.getItem('items');
-  }
 }
