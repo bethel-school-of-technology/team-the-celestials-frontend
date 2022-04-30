bf43451c3380645a0e45a059be552dfc9f640c9d
Harold Woody <89166105+HuckWoody@users.noreply.github.com>
2022-04-30 13:15:43 -0400

    Checkout button badge dynamically reflects # of items in cart

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Coffee } from '../models/coffee';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  public items: Coffee[];
+  public itemsInCart: number;
 
-  constructor() { }
+  constructor() {
+    setInterval(this.listCartnumber.bind(this), 500);
+  }
 
   ngOnInit(): void {
+    let coffeeString = localStorage.getItem('items');
+
+      if (coffeeString !== null) {
+
+      this.items = JSON.parse(coffeeString);
+
+      this.itemsInCart = this.items.length
+    }
+
   }
+  listCartnumber() {
+    let coffeeString = localStorage.getItem('items');
+
+      if (coffeeString !== null) {
+
+      this.items = JSON.parse(coffeeString);
+
+      this.itemsInCart = this.items.length
+      }
+  } 
 
 }
